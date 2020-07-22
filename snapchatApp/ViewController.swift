e1cb6c125bc8f6bbc34331bad3a2b7f95c595469
Erick Tshimbombo <erick.tshimbombo@yahoo.com>
2020-07-23 00:43:21 +0200

    Initial Commit

---
+++ snapchatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  snapchatApp
+//
+//  Created by El nino Cholo on 2020/07/23.
+//  Copyright © 2020 El nino Cholo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
